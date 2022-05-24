e76ba554596a1ff149c1d72d4c49200737978c91
Akash G M <97089350+AKASH79960@users.noreply.github.com>
2022-05-24 16:53:11 +0000

    09_done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,30 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+#filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+fh = open(fname)
+x= list()
+y=fh.read()
+words=y.split()
+
+
+for nfname = input("Enter file name: ")
+fh = open(fname)
+x= list()
+y=fh.read()
+words=y.split()
+
+
+for n in words: 
+    if n not in x:
+        x.append(n)
+        
+x.sort()
+print(x)
+
+ in words: 
+    if n not in x:
+        x.append(n)
+        
+x.sort()
+print(x)
